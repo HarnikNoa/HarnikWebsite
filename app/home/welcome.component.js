36c08efb7b7f80beccc6251731fd3db6ad7dab79
HarnikNoa <HarnikNoa@gmail.com>
2016-11-21 19:15:58 -0700

    Final changes

--- app/home/welcome.component.js
+++ app/home/welcome.component.js
@@ -11,7 +11,8 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var WelcomeComponent = (function () {
     function WelcomeComponent() {
-        this.pageTitle = 'Welcome to Noa\'s Site!';
+        this.pageTitle = 'Welcome to Noa\'s Lesson Emporium!';
+        this.subTitle = 'Computer science lessons for girls ages 9 - 12';
         this.notableWoman = "Ada Lovelace";
         this.notableWomanInfo = '"The daughter of famed poet Lord Byron, Augusta Ada Byron, Countess of Lovelace- better known as "Ada Lovelace"-was born in London on December 10, 1815. Ada showed her gift for mathematics at an early age. She translated an article on an invention by Charles Babbage, and added her own comments. Because she introduced many computer concepts, Ada is considered the first computer programmer. Ada died on November 27, 1852."';
     }
--- app/home/welcome.component.ts
+++ app/home/welcome.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
     templateUrl: 'app/home/welcome.component.html'
 })
 export class WelcomeComponent {
-    public pageTitle: string = 'Welcome to Noa\'s Site!';
+    public pageTitle: string = 'Welcome to Noa\'s Lesson Emporium!';
+    public subTitle: string = 'Computer science lessons for girls ages 9 - 12';
     public notableWoman: string = "Ada Lovelace";
     public notableWomanInfo: string = '"The daughter of famed poet Lord Byron, Augusta Ada Byron, Countess of Lovelace- better known as "Ada Lovelace"-was born in London on December 10, 1815. Ada showed her gift for mathematics at an early age. She translated an article on an invention by Charles Babbage, and added her own comments. Because she introduced many computer concepts, Ada is considered the first computer programmer. Ada died on November 27, 1852."';
 
